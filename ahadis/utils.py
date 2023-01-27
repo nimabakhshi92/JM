9f665113d3b6a7fb5df9d20077131ffff44e2904
nima bakhshi <nima.bakhshi@leaftech.eu>
2023-01-27 20:55:50 +0330

    narrations summary tree front

---
+++ ahadis/utils.py
@@ -0,0 +1,11 @@
+
+# DESKTOP-EO1NKE0
+# HYPERSIGNAL\SQLEXPRESS
+# conn = pyodbc.connect('Driver={SQL Server};'
+#                       'Server=DESKTOP-EO1NKE0;'
+#                       'Database=JanatolMava;'
+#                       'UID=MYLOGIN;'
+#                       'PWD=MYLOGIN;'
+#                       'Trusted_Connection=yes;')
+
+conn = 1
