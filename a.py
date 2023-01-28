acf5dc196c8f71f4ddaad7250ca989b75627ab5e
nima bakhshi <nima.bakhshi@leaftech.eu>
2023-01-28 16:04:16 +0330

    update

---
+++ a.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import mysql.connector
+
+# conn = mysql.connector.connect(host='localhost', database='JM', user='root', password='newpassword')
+df = pd.read_csv('./quran clean.csv')
+#
+# df.to_sql(con=conn, name='QuranVerseC', if_exists='replace', schema='JM')
+
+import sqlalchemy
+
+database_username = 'root'
+database_password = 'newpassword'
+database_ip = ''
+database_name = 'JM'
+database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                               format(database_username, database_password,
+                                                      database_ip, database_name))
+df.to_sql(con=database_connection, name='QuranVerse', if_exists='append', index=False)
